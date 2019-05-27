3c1b8cb29f51625246e9e9aa8d3c2ca1b9ff126a
Gatsby <z.goulding@hotmail.com>
2019-05-27 18:54:04 +1000

    Update #1
    Fixed a few errors & added the default App.js from 'create-react-app' starting screen.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App'; //React Entry point.
+import App from './App'; //React Entry point.
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
