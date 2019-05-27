4d491ae842c89a1eb5e86dba19bb4cbad16f96ca
Gatsby <z.goulding@hotmail.com>
2019-05-27 17:21:54 +1000

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+//Component rendered to index.js
+//Customize as you see fit. 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	
+
+render() {
+	return ();
+	}
+}
+
+
+
+export default App;
